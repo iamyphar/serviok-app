886b58c386cde8732ed85c1f6bdb85e4e78425d6
Raphaël Doutaz <raphael.doutaz@format-z.ch>
2023-11-16 22:19:29 +0100

    fix: error

---
+++ system/config/localconfig.php
@@ -0,0 +1,14 @@
+<?php
+
+### INSTALL SCRIPT START ###
+$GLOBALS['TL_CONFIG']['licenseAccepted'] = true;
+$GLOBALS['TL_CONFIG']['adminEmail'] = 'info@serviok.ch';
+$GLOBALS['TL_CONFIG']['maxFileSize'] = 314572800;
+$GLOBALS['TL_CONFIG']['dateFormat'] = 'd.m.Y';
+$GLOBALS['TL_CONFIG']['datimFormat'] = 'd.m.Y H:i';
+$GLOBALS['TL_CONFIG']['imageWidth'] = 3840;
+$GLOBALS['TL_CONFIG']['imageHeight'] = 4320;
+$GLOBALS['TL_CONFIG']['gdMaxImgWidth'] = 10000;
+$GLOBALS['TL_CONFIG']['gdMaxImgHeight'] = 10000;
+$GLOBALS['TL_CONFIG']['allowedTags'] = '<altGlyph><altGlyphDef><altGlyphItem><animate><animateColor><animateMotion><animateTransform><animation><audio><canvas><circle><clipPath><color-profile><cursor><defs><desc><discard><ellipse><feBlend><feColorMatrix><feComponentTransfer><feComposite><feConvolveMatrix><feDiffuseLighting><feDisplacementMap><feDistantLight><feDropShadow><feFlood><feFuncA><feFuncB><feFuncG><feFuncR><feGaussianBlur><feImage><feMerge><feMergeNode><feMorphology><feOffset><fePointLight><feSpecularLighting><feSpotLight><feTile><feTurbulence><filter><font><font-face><font-face-format><font-face-name><font-face-src><font-face-uri><foreignObject><g><glyph><glyphRef><handler><hatch><hatchpath><hkern><iframe><image><line><linearGradient><listener><marker><mask><mesh><meshgradient><meshpatch><meshrow><metadata><missing-glyph><mpath><path><pattern><polygon><polyline><prefetch><radialGradient><rect><script><set><solidColor><solidcolor><stop><style><svg><switch><symbol><tbreak><text><textArea><textPath><title><tref><tspan><unknown><use><video><view><vkern><a><abbr><acronym><address><area><article><aside><audio><b><bdi><bdo><big><blockquote><br><base><button><canvas><caption><cite><code><col><colgroup><data><datalist><dataset><dd><del><dfn><div><dl><dt><em><fieldset><figcaption><figure><footer><form><h1><h2><h3><h4><h5><h6><header><hgroup><hr><i><img><input><ins><kbd><keygen><label><legend><li><link><map><mark><menu><nav><object><ol><optgroup><option><output><p><param><picture><pre><q><s><samp><section><select><small><source><span><strong><style><sub><sup><table><tbody><td><textarea><tfoot><th><thead><time><tr><tt><u><ul><var><video><wbr>';
+### INSTALL SCRIPT STOP ###
