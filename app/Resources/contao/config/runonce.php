245ff6ec68350ee8ed103c2bf497bb306e168c22
Raphaël Doutaz <raphael.doutaz@format-z.ch>
2023-11-16 15:21:49 +0100

    init

---
+++ app/Resources/contao/config/runonce.php
@@ -0,0 +1,23 @@
+<?php
+
+$runonce = function(array $files, $delete = false) {
+    foreach ($files as $file) {
+        try {
+            include $file;
+        } catch (\Exception $e) {}
+
+        $relpath = str_replace(TL_ROOT . DIRECTORY_SEPARATOR, '', $file);
+
+        if ($delete && !unlink($file)) {
+            throw new \Exception(
+                'The file ' . $relpath . ' cannot be deleted. ' .
+                'Please remove the file manually and correct the file permission settings on your server.'
+            );
+        }
+    }
+};
+
+$runonce(array (
+  0 => '/Users/raphael.doutaz/Sites/serviok-app/vendor/codefog/contao-haste/config/upgrade.php',
+  1 => '/Users/raphael.doutaz/Sites/serviok-app/vendor/isotope/isotope-core/system/modules/isotope/config/upgrade.php',
+));
